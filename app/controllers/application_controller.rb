8728de2edcceae1cc88f99bf98f162065d4b4122
Yassi <yassih84@gmail.com>
2016-05-23 16:17:57 -0400

    CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,22 @@ require "application_responder"
 
 
 class ApplicationController < ActionController::Base
-
 	config.action_dispatch.default_headers.merge!({
   'Access-Control-Allow-Origin' => '*',
   'Access-Control-Request-Method' => '*'
 })
 
-# 	before_filter :add_allow_credentials_headers
+	before_filter :add_allow_credentials_headers
 
-# 	def add_allow_credentials_headers                                                                                                                                                                                                                                                        
-#   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
-#   #                                                                                                                                                                                                                                                                                       
-#   # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
-#   # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
-#   # the browser will not reject the response                                                                                                                                                                                                                                             
-#   response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-#   response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
-# end
+	def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
+  #                                                                                                                                                                                                                                                                                       
+  # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
+  # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
+  # the browser will not reject the response                                                                                                                                                                                                                                             
+  response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+end
 
 
 
