6686a6506ae0c8f47c6d222e76d9866c76d63346
Yassi <yassih84@gmail.com>
2016-05-23 15:19:55 -0400

    CORS ISSUE

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+# ruby '2.2.4'
 gem 'cocoon'
 gem 'responders'
 gem 'react-rails', '~> 1.5.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,31 @@
 require "application_responder"
 
+
 class ApplicationController < ActionController::Base
 
-	after_filter :cors_set_access_control_headers
+	before_filter :add_allow_credentials_headers
 
-	def cors_set_access_control_headers
-	  headers['Access-Control-Allow-Origin'] = '*'
-	  headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-	  headers['Access-Control-Allow-Headers'] = '*'
-	  headers['Access-Control-Request-Method'] = '*'
-	  headers['Access-Control-Max-Age'] = "1728000"
-	end
+	def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
+  #                                                                                                                                                                                                                                                                                       
+  # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
+  # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
+  # the browser will not reject the response                                                                                                                                                                                                                                             
+  response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+end
+
+
+
+	# after_filter :cors_set_access_control_headers
+
+	# def cors_set_access_control_headers
+	#   headers['Access-Control-Allow-Origin'] = '*'
+	#   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+	#   headers['Access-Control-Allow-Headers'] = '*'
+	#   headers['Access-Control-Request-Method'] = '*'
+	#   headers['Access-Control-Max-Age'] = "1728000"
+	# end
 
 	  self.responder = ApplicationResponder
 	  respond_to :html
@@ -21,5 +36,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
 	# protect_from_forgery
 	skip_before_action :verify_authenticity_token, only: [:create]
+
+
 
 end
